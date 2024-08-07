fa949e9c2b0e2de40fa47914a3330bd1974d3dd1
Param Desai <93168030+ParamDesai111@users.noreply.github.com>
2024-08-07 02:15:07 +0000

    queries

--- SQLSetup/Queries.sql
+++ SQLSetup/Queries.sql
@@ -2,3 +2,11 @@ SELECT *
 FROM PlayerStatistics AS PLS
 JOIN Players AS P ON PLS.playerId = P.playerId
 WHERE P.playerName = 'Luka Doncic';
+
+(SELECT * FROM PlayerStatistics WHERE season < 2024)
+
+SELECT points, season FROM PlayerStatistics
+WHERE playerId = 'doncilu01'
+
+SELECT * FROM Players
+WHERE playerId = 'yurtsom01'
