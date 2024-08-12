3e787017bda54521a9079b9377cd626d0a66a3c6
Param Desai <93168030+ParamDesai111@users.noreply.github.com>
2024-08-12 20:16:09 +0000

    powerbi initial setup

--- webapp/frontend/src/components/PowerBIDashboard.js
+++ webapp/frontend/src/components/PowerBIDashboard.js
@@ -8,7 +8,8 @@ function PowerBIDashboard() {
                 title="Power BI"
                 width="100%"
                 height="600"
-                src="https://app.powerbi.com/view?r=YOUR_EMBED_LINK"
+                // src="https://app.powerbi.com/view?r=YOUR_EMBED_LINK"
+                src = "https://app.powerbi.com/view?r=YOUR_EMBED_LINK"
                 frameBorder="0"
                 allowFullScreen={true}
             ></iframe>
